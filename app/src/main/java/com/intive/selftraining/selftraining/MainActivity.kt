3a24cbad02c69ec5053cbf9fd7baff6219360a67
arthur.oliveira <arthurabreupro@gmail.com>
2018-11-08 12:02:32 +0100

    fixed error with koin in MainActivity

--- app/src/main/java/com/intive/selftraining/selftraining/MainActivity.kt
+++ app/src/main/java/com/intive/selftraining/selftraining/MainActivity.kt
@@ -3,16 +3,12 @@ package com.intive.selftraining.selftraining
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.intive.selftraining.selftraining.module.appmodule
 import com.intive.selftraining.selftraining.view.ListMoviesActivity
-import org.koin.android.ext.android.startKoin
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        startKoin(this, listOf(appmodule))
 
         title = "MainActivity"
         startActivity(Intent(this, ListMoviesActivity::class.java))
