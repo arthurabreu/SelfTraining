6e922fd48883ac84151212c4dcb8bf38eb7622eb
arthur.oliveira <arthurabreupro@gmail.com>
2018-11-20 11:14:49 +0100

    removed unused references in appmodule

--- app/src/main/java/com/intive/selftraining/selftraining/di/AppModule.kt
+++ app/src/main/java/com/intive/selftraining/selftraining/di/AppModule.kt
@@ -1,7 +1,5 @@
 package com.intive.selftraining.selftraining.di
 
-import com.intive.selftraining.selftraining.detailsmovies.DetailsRespository
-import com.intive.selftraining.selftraining.detailsmovies.DetailsScopeViewModel
 import com.intive.selftraining.selftraining.listmovies.ListMoviesRepository
 import com.intive.selftraining.selftraining.listmovies.ListMoviesViewModel
 import com.intive.selftraining.selftraining.network.NetworkClient
@@ -11,8 +9,6 @@ import org.koin.dsl.module.module
 val appmodule = module {
     single { NetworkClient() }
     single { ListMoviesRepository(get()) }
-    single { DetailsRespository() }
 
-    scope("movies") { DetailsScopeViewModel(get()) }
     viewModel { ListMoviesViewModel(get()) }
 }
