0689380281fc53ddcc0f7796c1b137242996799c
Alexander Tumanov <alvtum@gmail.com>
2018-11-21 14:54:17 +0100

    mock of livedata

--- app/src/test/java/com/intive/selftraining/selftraining/listmovies/ListMoviesViewModelTest.kt
+++ app/src/test/java/com/intive/selftraining/selftraining/listmovies/ListMoviesViewModelTest.kt
@@ -1,35 +1,54 @@
 package com.intive.selftraining.selftraining.listmovies
 
+import android.arch.lifecycle.MutableLiveData
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.intive.selftraining.selftraining.network.models.Configuration
+import com.intive.selftraining.selftraining.network.models.Images
 import com.intive.selftraining.selftraining.network.models.MoviesResponse
 import org.junit.Test
 import org.junit.Rule
 import com.intive.selftraining.selftraining.network.models.Result
 import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import org.amshove.kluent.`should equal`
 import org.junit.rules.TestRule
+import org.junit.Before
 
 class ListMoviesViewModelTest {
 
-    var listMoviesViewModel = mock<ListMoviesViewModel>()
+    var resultsList: MutableLiveData<ZipListMovies> = mock()
 
     @Rule
     @JvmField
     val rule: TestRule = InstantTaskExecutorRule()
 
-    @Test
-    fun getMoviesResponse() {
-
-        /* Given */
-        val response = getResponse()
-
-        /* When */
-//        whenever(listMoviesViewModel.getMoviesResponse()).thenReturn(response)
-
-        /* Then */
-//        listMoviesViewModel.getMoviesResponse() `should equal` response
+    @Before
+    fun setUp() {
+        whenever(resultsList.value).thenReturn(getZipListMovies())
     }
 
-    private fun getResponse(): MoviesResponse {
+    @Test
+    fun `when should assert zipListMovies`() {
+        resultsList.value `should equal` getZipListMovies()
+    }
+
+    private fun getZipListMovies(): ZipListMovies {
+
+        val img = Images(
+            emptyList(),
+            "http://image.tmdb.org/t/p/",
+            listOf("1", "2", "3", "4", "5", "6"),
+            emptyList(),
+            emptyList(),
+            "",
+            emptyList()
+        )
+        val conf = Configuration(emptyList(), img)
+        return ZipListMovies(getMovieResponse(), conf)
+    }
+
+    private fun getMovieResponse(): MoviesResponse {
+
         val result = Result(
             false, "/VuukZLgaCrho2Ar8Scl9HtV3yD.jpg", listOf(878), 335983, "en", "Venom",
             "When Eddie Brock acquires the powers of a symbiote, he will have to release his alter-ego \"Venom\" to save his life.",
