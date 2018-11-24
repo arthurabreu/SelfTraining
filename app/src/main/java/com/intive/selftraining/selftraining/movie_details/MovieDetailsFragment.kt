e8c19d5576b2a08f25ecee414795e0ca9088844e
Alexander Tumanov <alvtum@gmail.com>
2018-11-24 19:28:44 +0100

    added movie fragment

---
+++ app/src/main/java/com/intive/selftraining/selftraining/movie_details/MovieDetailsFragment.kt
@@ -0,0 +1,25 @@
+package com.intive.selftraining.selftraining.movie_details
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.intive.selftraining.selftraining.R
+
+class MovieDetailsFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.activity_details, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+    }
+}
