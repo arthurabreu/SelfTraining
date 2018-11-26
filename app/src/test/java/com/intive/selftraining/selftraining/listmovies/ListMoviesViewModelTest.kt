ee3d95bf285e378761a0df2e069cedf0dc1057e8
arthur.oliveira <arthurabreupro@gmail.com>
2018-11-26 15:27:55 +0100

    fixed error on test but didn't fix the test properly yet

--- app/src/test/java/com/intive/selftraining/selftraining/listmovies/ListMoviesViewModelTest.kt
+++ app/src/test/java/com/intive/selftraining/selftraining/listmovies/ListMoviesViewModelTest.kt
@@ -2,19 +2,18 @@ package com.intive.selftraining.selftraining.listmovies
 
 import android.arch.lifecycle.MutableLiveData
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import com.intive.selftraining.selftraining.network.models.ApiConfiguration
-import com.intive.selftraining.selftraining.network.models.Images
-import com.intive.selftraining.selftraining.network.models.MoviesResponseEntity
-import org.junit.Test
-import org.junit.Rule
-import com.intive.selftraining.selftraining.network.models.MoviesEntity
+import com.intive.selftraining.selftraining.listmovies.model.Movies
+import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
+import org.amshove.kluent.`should equal`
+import org.junit.Rule
+import org.junit.Test
 import org.junit.rules.TestRule
 
 class ListMoviesViewModelTest {
 
-    var resultsList: MutableLiveData<ZipListMovies> = mock {
-        on { value } doReturn getZipListMovies()
+    var resultsList: MutableLiveData<List<Movies>> = mock {
+        on { value } doReturn getMovieResponse()
     }
 
     @Rule
@@ -22,39 +21,36 @@ class ListMoviesViewModelTest {
     val rule: TestRule = InstantTaskExecutorRule()
 
     @Test
-    fun `when should assert zipListMovies`() {
-        resultsList.value `should equal` getZipListMovies()
+    fun `when should assert Movies`() {
+        resultsList.value `should equal` getMovieResponse()
     }
 
-    private fun getZipListMovies(): ZipListMovies {
 
-        val img = Images(
-            emptyList(),
-            "http://image.tmdb.org/t/p/",
-            listOf("1", "2", "3", "4", "5", "6"),
-            emptyList(),
-            emptyList(),
-            "",
-            emptyList()
-        )
-        val conf = ApiConfiguration(emptyList(), img)
-        return ZipListMovies(getMovieResponse(), conf)
-    }
+    private fun getMovieResponse(): List<Movies> {
 
-    private fun getMovieResponse(): MoviesResponseEntity {
+        val numbers: MutableList<Int> = mutableListOf(1, 2, 3)
+        val readOnlyList: List<Int> = numbers
+        val mutList = mutableListOf<Movies>()
 
-        val result = MoviesEntity(
-            false, "/VuukZLgaCrho2Ar8Scl9HtV3yD.jpg", listOf(878), 335983, "en", "Venom",
-            "When Eddie Brock acquires the powers of a symbiote, he will have to release his alter-ego \"Venom\" to save his life.",
-            228.224,
-            "/2uNW4WbgBXL25BAbXGLnLqX71Sw.jpg",
-            "2018-10-03",
-            "Venom",
-            false,
-            6.6,
-            2192
-        )
+        var movies = Movies()
+        movies.id = 1
+        movies.title = "Fantastic Beasts: The Crimes of Grindelwald"
+        movies.releaseDate = "2018-11-14"
+        movies.posterPath = "/uyJgTzAsp3Za2TaPiZt2yaKYRIR.jpg"
+        movies.adult = false
+        movies.backdropPath = "/xgbeBCjmFpRYHDF7tQ7U98EREWp.jpg"
+        movies.genreIds = readOnlyList
+        movies.originalLanguage = "en"
+        movies.originalTitle = "Fantastic Beasts: The Crimes of Grindelwald"
+        movies.overview = "Gellert Grindelwald has escaped imprisonment and has begun gathering followers..."
+        movies.popularity = 332.518
+        movies.video = false
+        movies.voteAverage = 7.1
+        movies.voteCount = 1319
+        movies.completeImageUrl = "/xgbeBCjmFpRYHDF7tQ7U98EREWp.jpg"
 
-        return MoviesResponseEntity(1, listOf(result), 19401, 388003)
+        mutList.add(movies)
+
+        return mutList
     }
 }
