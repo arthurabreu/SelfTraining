c0700b20aa654a8a6085c74201871b9de5c6f71b
arthur.oliveira <arthurabreupro@gmail.com>
2018-12-03 13:45:44 +0100

    added placeholder to glide

--- app/src/main/java/com/intive/selftraining/selftraining/listmovies/adapter/ImageBindingAdapter.kt
+++ app/src/main/java/com/intive/selftraining/selftraining/listmovies/adapter/ImageBindingAdapter.kt
@@ -3,12 +3,17 @@ package com.intive.selftraining.selftraining.listmovies.adapter
 import android.databinding.BindingAdapter
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.intive.selftraining.selftraining.R
 
 @BindingAdapter("imgUrl")
 fun ImageView.loadImage(url: String?) {
     url?.let {
         Glide.with(context)
             .load(url)
+            .apply(
+                RequestOptions()
+                .placeholder(R.drawable.ic_launcher_background))
             .into(this)
     }
 }
