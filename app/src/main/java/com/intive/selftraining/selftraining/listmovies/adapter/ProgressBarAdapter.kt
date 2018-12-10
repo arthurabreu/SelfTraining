27e4d8ead75c524d96a81015f39e27c48d8b99e4
Alexander Tumanov <alvtum@gmail.com>
2018-12-10 16:24:08 +0100

    data binding - safeUnbox warning

--- app/src/main/java/com/intive/selftraining/selftraining/listmovies/adapter/ProgressBarAdapter.kt
+++ app/src/main/java/com/intive/selftraining/selftraining/listmovies/adapter/ProgressBarAdapter.kt
@@ -5,6 +5,6 @@ import android.view.View
 import android.widget.ProgressBar
 
 @BindingAdapter("progressBarVisibility")
-fun ProgressBar.visible(isVisible: Boolean) {
-    visibility = if (isVisible) View.INVISIBLE else View.VISIBLE
+fun ProgressBar.visible(isVisible: Boolean?) {
+    isVisible?.let { visibility = if (isVisible) View.INVISIBLE else View.VISIBLE }
 }
