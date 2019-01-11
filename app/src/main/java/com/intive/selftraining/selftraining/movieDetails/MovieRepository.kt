90e1cce425f00b60437911f017116023ddd8755d
Alexander Tumanov <alvtum@gmail.com>
2019-01-11 11:49:06 +0100

    adeed insert and getMovieById in MovieDetailsDatabase and moved there coroutines

--- app/src/main/java/com/intive/selftraining/selftraining/movieDetails/MovieRepository.kt
+++ app/src/main/java/com/intive/selftraining/selftraining/movieDetails/MovieRepository.kt
@@ -1,7 +1,7 @@
 package com.intive.selftraining.selftraining.movieDetails
 
 import com.intive.selftraining.selftraining.data.MovieDetailsMapper
-import com.intive.selftraining.selftraining.movieDetails.model.dao.MovieDetailsDao
+import com.intive.selftraining.selftraining.movieDetails.model.MovieDetailsDatabase
 import com.intive.selftraining.selftraining.movieDetails.model.enities.MovieDetails
 import com.intive.selftraining.selftraining.network.NetworkInterface
 import com.intive.selftraining.selftraining.network.models.listMovies.ConfigurationEntity
@@ -9,10 +9,8 @@ import com.intive.selftraining.selftraining.network.models.movieDetails.MovieDet
 import io.reactivex.Observable
 import io.reactivex.Single
 import io.reactivex.rxkotlin.Observables
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
-class MovieRepository(private val networkClient: NetworkInterface, private val movieDatabase: MovieDetailsDao) {
+class MovieRepository(private val networkClient: NetworkInterface, private val movieDatabase: MovieDetailsDatabase) {
     fun getMovieDetails(id: Int): Observable<MovieDetails> {
         return Observables.zip(showMovieDetails(id), getConfiguration()) { movie, configuration ->
             MovieDetailsMapper().mapFromEntity(movie, configuration.images)
@@ -23,10 +21,8 @@ class MovieRepository(private val networkClient: NetworkInterface, private val m
 
     private fun getConfiguration(): Observable<ConfigurationEntity> = networkClient.getConfiguration()
 
-    suspend fun addMovieToDB(movieDetails: MovieDetails) {
-        withContext(Dispatchers.IO) {
-            movieDatabase.insert(movieDetails)
-        }
+    fun addMovieToDB(movieDetails: MovieDetails) {
+        movieDatabase.insert(movieDetails)
     }
 
     fun readMovie(movieId: Int): Single<MovieDetails> {
