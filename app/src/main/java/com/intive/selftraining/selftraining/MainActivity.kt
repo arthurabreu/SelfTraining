04ab203b2534111966024cff16771e46f9cee321
arthur.oliveira <arthurabreupro@gmail.com>
2019-01-18 11:25:50 +0100

    added viewModel for Search

--- app/src/main/java/com/intive/selftraining/selftraining/MainActivity.kt
+++ app/src/main/java/com/intive/selftraining/selftraining/MainActivity.kt
@@ -6,17 +6,25 @@ import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.intive.selftraining.selftraining.databinding.ActivityMainBinding
+import com.intive.selftraining.selftraining.di.observeLifecycleIn
+import com.intive.selftraining.selftraining.search.SearchViewModel
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
     private var navController: NavController? = null
-    var binding: ActivityMainBinding? = null
+    private val viewModel: SearchViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding= DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding?.showSearch = false
+        this.observeLifecycleIn(viewModel)
+        var binding: ActivityMainBinding? = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding?.run {
+            this.searchViewModel = viewModel
+            setLifecycleOwner(this@MainActivity)
+        }
 
         navController = Navigation.findNavController(this, R.id.nav_host_fragment)
         navController?.navigate(R.id.fragment_list_movies)
