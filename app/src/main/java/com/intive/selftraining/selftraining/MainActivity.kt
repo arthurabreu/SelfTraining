120d568a43e6f810d1ce9bd043cd31a98ab02697
arthur.oliveira <arthurabreupro@gmail.com>
2019-01-18 11:05:24 +0100

    changed the implementation of menu. TODO: Create view model for the toolbar

--- app/src/main/java/com/intive/selftraining/selftraining/MainActivity.kt
+++ app/src/main/java/com/intive/selftraining/selftraining/MainActivity.kt
@@ -10,31 +10,15 @@ import com.intive.selftraining.selftraining.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private var navController: NavController? = null
-    //private val searchViewModel: SearchViewModel by viewModel()
+    var binding: ActivityMainBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        var binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.title = "TEST"
+        binding= DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding?.showSearch = false
 
         navController = Navigation.findNavController(this, R.id.nav_host_fragment)
         navController?.navigate(R.id.fragment_list_movies)
     }
-
-//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-//        menuInflater.inflate(R.menu.menu_main, menu)
-//
-//        val searchView = menu?.findItem(R.id.action_search)?.actionView as SearchView
-//        searchView.queryHint = getString(R.string.search)
-//        searchView?.setOnQueryTextListener(searchViewModel)
-//        searchView.setIconifiedByDefault(false)
-//        searchView.setOnSearchClickListener {
-//            Navigation.findNavController(this, R.id.nav_host_fragment).navigate(R.id.searchFragment)
-//        }
-//
-//        return true
-//    }
-
-    //TODO create toolbar binding adapter and a boolean field to show or hide the SearchView when I want and do the querylistener in the adapter
 }
